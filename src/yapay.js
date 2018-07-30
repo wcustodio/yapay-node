1d631bb8b3113cce3cd79b178b68acc5550408c6
Willian Custódio <wrafhael@gmail.comgit statusexitgit config --global user.email wrafhael@gmail.com>
2018-07-30 14:31:08 -0300

    Adding constructor

--- src/yapay.js
+++ src/yapay.js
@@ -1,4 +1,11 @@
-const yapay = function() {
+const yapay = function(params) {
+    this.token = params.token;
+    this.mode = params.sandbox == true ? 'sandbox' : 'prod';
+
+    switch (this.mode) {
+        case 'prod': this.url = 'http://api.intermediador.yapay.com.br/v1'; break;
+        case 'sandbox': this.url = 'https://api.intermediador.sandbox.yapay.com.br/v1'; break;
+    }
 }
 
 module.exports = yapay;
