501298dc0b4496e4c18cf0bbbb7db305352fe5a5
Willian Custódio <wrafhael@gmail.comgit statusexitgit config --global user.email wrafhael@gmail.com>
2019-02-27 11:05:31 -0300

    Update URL to HTTPS

--- src/yapay.js
+++ src/yapay.js
@@ -8,7 +8,7 @@ const yapay = function(params) {
     this.mode = params.sandbox == true ? 'sandbox' : 'prod';
 
     switch (this.mode) {
-        case 'prod': this.url = 'http://api.intermediador.yapay.com.br'; break;
+        case 'prod': this.url = 'https://api.intermediador.yapay.com.br'; break;
         case 'sandbox': this.url = 'https://api.intermediador.sandbox.yapay.com.br'; break;        
     }
 
